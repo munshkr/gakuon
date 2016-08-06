72acadd6a8a494506985fa72569caafb0665f317
Damián Silvani <munshkr@gmail.com>
2016-08-06 00:15:59 -0300

    Use Assembler to build .sid or .prg file on executable

--- bin/gakuon.js
+++ bin/gakuon.js
@@ -2,17 +2,16 @@
 const process = require('process');
 const fs = require('fs');
 const path = require('path');
-const PEG = require('pegjs');
 
 const compile = require('../lib/compiler').compile;
-const defaultOut = './song.asm';
+const Assembler = require('6502asm').Assembler;
+const defaultOut = './song.sid';
 
 let program = require('commander');
 
 program
   .usage('[options] <file>')
-  .option('-o, --output [file]',
-    `output file (default: ${defaultOut})`, defaultOut)
+  .option('-o, --output [file]', `output file`)
   .option('-s, --source', 'generate assembly source code')
   .option('--player',
     'generate a C64 executable player .prg instead of .sid file')
@@ -33,20 +32,53 @@ if (program.args.length > 1) {
   process.exit(1);
 }
 
-let inputPath = program.args[0];
-let outputPath = program.output;
 
-const source = fs.readFileSync(inputPath).toString();
-
-const templatePath = path.join(__dirname, program.player ?
-  '../lib/player/prg_player.asm' : '../lib/player/sid_player.asm');
-const template = fs.readFileSync(templatePath).toString();
-
-let out = compile(source, { template: template });
-
-if (program.source) {
-  fs.writeFileSync(outputPath, out);
-} else {
-  console.error('Assembler not implemented yet. Use --source for now.');
+function abort(err) {
+  if (err.location) {
+    let loc = err.location.start;
+    console.error(`${err.message}, at ${loc.line}:${loc.column}`);
+  } else {
+    console.error(err);
+  }
   process.exit(1);
+}
+
+function outputPathFromOptions(program) {
+  let inputPath = program.args[0];
+  let basename = path.basename(inputPath, path.extname(inputPath));
+
+  let newExt;
+  if (program.source) {
+    newExt = '.asm';
+  } else if (program.player) {
+    newExt = '.prg';
+  } else {
+    newExt = '.sid';
+  }
+
+  return `${basename}${newExt}`;
+}
+
+let inputPath = program.args[0];
+let outputPath = program.output || outputPathFromOptions(program);
+
+try {
+  const source = fs.readFileSync(inputPath).toString();
+
+  const templatePath = path.join(__dirname, program.player ?
+    '../lib/player/prg_player.asm' : '../lib/player/sid_player.asm');
+  const template = fs.readFileSync(templatePath).toString();
+
+  let out = compile(source, { template: template });
+
+  if (program.source) {
+    fs.writeFileSync(outputPath, out);
+  } else {
+    let assembler = new Assembler();
+    let {objectCode} = assembler.assemble(out);
+    let buf = Buffer.from(objectCode);
+    fs.writeFileSync(outputPath, buf);
+  }
+} catch(err) {
+  abort(err);
 }
